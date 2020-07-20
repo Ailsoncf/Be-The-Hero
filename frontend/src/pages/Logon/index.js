bd05cf0cb1225cc4986802d5b743599442d67273
Ailsoncf <ailson_cf@yahoo.com.br>
2020-07-20 14:00:43 +1200

    front end

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,54 +1,57 @@
-import React, { useState }from 'react'
+import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
-import { FiLogIn } from  'react-icons/fi'
+import { FiLogIn } from 'react-icons/fi'
 import api from '../../services/api'
 
 import './styles.css'
-import heroesImg from '../../assets/heroes.png' 
+import heroesImg from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
 
 export default function Logon() {
-    const [ id, setId] = useState()
-    const history = useHistory()
+  const [id, setId] = useState()
+  const history = useHistory()
 
-    async function handleLogin(e) {
-        e.preventDefault()
+  async function handleLogin(e) {
+    e.preventDefault()
 
-        try {
-            const response = await api.post('sessions', { id })
+    try {
+      const response = await api.post('sessions', { id })
 
-            localStorage.setItem('ongId', id)
-            localStorage.setItem('ongName', response.data.name)
+      localStorage.setItem('ongId', id)
+      localStorage.setItem('ongName', response.data.name)
 
-            history.push('/profile')
-        } catch(err) {
-            alert('Falha no Login, tente novamente')
-        }
+      history.push('/profile')
+    } catch (err) {
+      alert('Falha no Login, tente novamente')
     }
+  }
 
-    return (
-        <div className="logon-container">
-            <section className="form">
-                <img src={ logoImg } alt="Be the Hero"/>
-                
-            <form onSubmit={ handleLogin }>
-                <h1>Faça seu logon</h1>
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be the Hero" />
 
-                <input 
-                    placeholder="Sua ID"
-                    valeu={id}
-                    onChange={ e => setId(e.target.value) }
-                />
-                <button className="button" type="submit"> Entrar </button>
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu logon</h1>
 
-                <Link className="back-link" to="/register">
-                    <FiLogIn size={16} color="#e02041"/>
-                    Não tenho cadastro
-                </Link>
-            </form>
-            </section>
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <button className="button" type="submit">
+            {' '}
+            Entrar{' '}
+          </button>
 
-            <img src={ heroesImg } alt="heroes" />
-        </div>
-    )
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt="heroes" />
+    </div>
+  )
 }
