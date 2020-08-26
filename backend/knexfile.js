34467259785266344e1c15dea711fce0230f3c0b
ailsoncf <ailson_cf@yahoo.com.br>
2020-08-26 17:24:14 +1200

    Refactoring knex connection

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,48 +1,12 @@
-// Update with your config settings.
+const path = require('path')
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './src/database/db.sqlite'
-    },
-    migration: {
-      diretory: '.src/database/migrations'
-    },
-    useNullAsDefault: true
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+  useNullAsDefault: true,
+}
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,12 @@
 const knex = require('knex')
-const configuration = require('../../knexfile')
+const path = require('path')
 
-const connection = knex(configuration.development)
+const connection = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'database.sqlite'),
+  },
+  useNullAsDefault: true,
+})
 
 module.exports = connection
